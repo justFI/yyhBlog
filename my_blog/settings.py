faa270750a085e52d5a04eac9fee69a8f741e73a
杨辉 <asd3.1@qq.com>
2019-02-02 17:06:54 +0800

    sqlite3 数据迁移到 mysql

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_blog_db',
+        'USER': 'root',
+        'PASSWORD': 'yyh.mysql',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
